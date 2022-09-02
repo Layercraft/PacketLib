518da1c53976ee4c61f229d402a1f2944025d938
Julian Haag <me@jhaag.me>
2022-09-02 15:07:28 +0200

    Remove inline Methods

--- src/main/kotlin/io/layercraft/translator/utils/VarString.kt
+++ src/main/kotlin/io/layercraft/translator/utils/VarString.kt
@@ -1,9 +1,7 @@
 package io.layercraft.translator.utils
 
 import io.layercraft.translator.utils.MinecraftVarInt.fromVarInt
-import io.layercraft.translator.utils.MinecraftVarInt.readVarInt
 import io.layercraft.translator.utils.MinecraftVarInt.toVarInt
-import io.layercraft.translator.utils.MinecraftVarInt.writeVarInt
 import java.lang.RuntimeException
 
 const val MINECRAFT_MAX_STRING_LENGTH = 32767
@@ -44,44 +42,5 @@ object MinecraftString {
             //throw MinecraftProtocolDecodingException("String too big (was " + length + " bytes encoded, max " + MINECRAFT_MAX_STRING_LENGTH + ")")
         }
         return toVarInt(bytes.size) + bytes
-    }
-
-    inline fun readString(
-        maxLength: Int,
-        readByte: () -> Byte,
-        readBytes: (length: Int) -> ByteArray,
-    ): String {
-        val length: Int = readVarInt(readByte)
-        return if (length > maxLength * 4) {
-            throw RuntimeException()
-            //throw MinecraftProtocolDecodingException("The received encoded string buffer length is longer than maximum allowed (" + length + " > " + maxLength * 4 + ")")
-        } else if (length < 0) {
-            throw RuntimeException()
-            //throw MinecraftProtocolDecodingException("The received encoded string buffer length is less than zero! Weird string!")
-        } else {
-            val stringBuffer = readBytes(length).decodeToString()
-            if (stringBuffer.length > maxLength) {
-                throw RuntimeException()
-                //throw MinecraftProtocolDecodingException("The received string length is longer than maximum allowed ($length > $maxLength)")
-            } else {
-                stringBuffer
-            }
-        }
-    }
-
-    inline fun writeString(
-        string: String,
-        writeByte: (Byte) -> Unit,
-        writeFully: (ByteArray) -> Unit
-    ) {
-        val bytes = string.toByteArray()
-
-        if (bytes.size > MINECRAFT_MAX_STRING_LENGTH) {
-            throw RuntimeException()
-            //throw MinecraftProtocolEncodingException("String too big (was " + bytes.size + " bytes encoded, max " + 32767 + ")")
-        } else {
-            writeVarInt(bytes.size, writeByte)
-            writeFully(bytes)
-        }
     }
 }
