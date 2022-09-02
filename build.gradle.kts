d5baf684713eda7c602bc2b91289b40045d0183c
Julian Haag <me@jhaag.me>
2022-09-02 11:18:14 +0200

    Init Project

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.7.10"
+    kotlin("plugin.serialization") version "1.7.10"
+}
+
+group = "io.layercraft.translator"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.0")
+
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name = "Translator-API"
+
