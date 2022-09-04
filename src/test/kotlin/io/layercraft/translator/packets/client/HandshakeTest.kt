c41e674e3ccdce891e9df7da2f0391d17cacd66a
Julian Haag <me@jhaag.me>
2022-09-04 17:15:16 +0200

    Add Tests
    Tests for commit 5f73d82f

--- src/test/kotlin/io/layercraft/translator/packets/client/HandshakeTest.kt
+++ src/test/kotlin/io/layercraft/translator/packets/client/HandshakeTest.kt
@@ -2,13 +2,13 @@ package io.layercraft.translator.packets.client
 
 import io.layercraft.translator.ProtocolVersion
 import io.layercraft.translator.TranslatorAPI
-import kotlinx.serialization.InternalSerializationApi
+import io.layercraft.translator.packets.server.login.EncryptionRequest
+import io.layercraft.translator.utils.MinecraftVarIntUtils
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 internal class HandshakeTest{
 
-    @InternalSerializationApi
     @Test
     fun testHandshakePacket(){
 
@@ -20,5 +20,24 @@ internal class HandshakeTest{
         val decoded = TranslatorAPI.decodeFromByteArray(Handshake.serializer(), bytes)
 
         assertEquals(packet, decoded)
+    }
+
+    @Test
+    fun testByteArray(){
+
+        val exampleByteArray = byteArrayOf(0x04, 0x6C, 0x6F, 0x63, 0x61, 0x6C, 0x68, 0x6F, 0x73, 0x74, 0x00, 0x7F, 0x00, 0x00)
+
+        val packet =
+            EncryptionRequest(
+                "",
+                exampleByteArray,
+                exampleByteArray
+            )
+
+        val bytes = TranslatorAPI.encodeToByteArray(EncryptionRequest.serializer(), packet)
+
+        val decoded = TranslatorAPI.decodeFromByteArray(EncryptionRequest.serializer(), bytes)
+
+        assertEquals(packet.hashCode(), decoded.hashCode())
     }
 }
