b0ee6c98a534a8de9d7abcad5cec7c89b2f4d4c2
Julian Haag <me@jhaag.me>
2022-09-09 16:46:21 +0200

    Update Types and remove useless

--- src/main/kotlin/io/layercraft/translator/types/Position.kt
+++ src/main/kotlin/io/layercraft/translator/types/Position.kt
@@ -1,14 +1,5 @@
 package io.layercraft.translator.types
 
-import kotlinx.serialization.ExperimentalSerializationApi
-import kotlinx.serialization.KSerializer
-import kotlinx.serialization.Serializable
-import kotlinx.serialization.Serializer
-import kotlinx.serialization.descriptors.SerialDescriptor
-import kotlinx.serialization.descriptors.buildClassSerialDescriptor
-import kotlinx.serialization.descriptors.element
-import kotlinx.serialization.encoding.*
-
 internal fun Double.toPositionInt(): Int = (this * 32.0).toInt()
 internal fun Int.toPositionDouble(): Double = this / 32.0
 
@@ -31,47 +22,13 @@ fun Position(
  * @property z
  * @see <a href="https://wiki.vg/Protocol#Position">https://wiki.vg/Protocol#Position</a>
  */
-@Serializable
 data class Position(
     val x: Int,
     val y: Int,
     val z: Int
 ) {
 
-    @Serializer(forClass = Position::class)
-    companion object : KSerializer<Position> {
-        override val descriptor: SerialDescriptor =
-            buildClassSerialDescriptor("io.layercraft.translator.types.Position") {
-                element<Long>("position")
-            }
-
-        @OptIn(ExperimentalSerializationApi::class)
-        override fun deserialize(decoder: Decoder): Position =
-            decoder.decodeStructure(descriptor) {
-                var positionContent: Long? = null
-
-                if (decodeSequentially()) { // sequential decoding protocol
-                    positionContent = decodeLongElement(descriptor, 0)
-                } else while (true) {
-                    when (val index = decodeElementIndex(descriptor)) {
-                        0 -> positionContent = decodeLongElement(descriptor, 0)
-                        CompositeDecoder.DECODE_DONE -> break
-                        else -> error("Unexpected index: $index")
-                    }
-                }
-
-                requireNotNull(positionContent)
-
-                longToPosition(positionContent)
-            }
-
-
-        override fun serialize(encoder: Encoder, value: Position) {
-            encoder.encodeStructure(descriptor) {
-                encodeLongElement(descriptor, 0, positionToLong(value))
-            }
-        }
-
+    companion object {
         fun positionToLong(position: Position): Long = with(position) {
             x.toLong() and 0x3FFFFFF shl 38 or (z.toLong() and 0x3FFFFFF shl 12) or (y.toLong() and 0xFFF)
         }
