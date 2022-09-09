72fdb7f34f7af14215d9e872e9e65dc3227e5e68
Julian Haag <me@jhaag.me>
2022-09-09 16:43:08 +0200

    Change Packets Interface

--- src/main/kotlin/io/layercraft/translator/packets/Packet.kt
+++ src/main/kotlin/io/layercraft/translator/packets/Packet.kt
@@ -1,7 +1,31 @@
 package io.layercraft.translator.packets
 
+import io.ktor.utils.io.core.*
+
+
 interface Packet
 
-interface ServerPacket : Packet
+interface ClientPacket: Packet
+interface ServerPacket: Packet
 
-interface ClientPacket : Packet
+
+interface PacketSerializer<T> where T: Packet {
+    fun serialize(input: Input): T
+
+    fun deserialize(output: Output, value: T)
+}
+
+
+annotation class MinecraftPacket(val packetId: Int, val state: PacketState, val direction: PacketDirection)
+
+enum class PacketDirection {
+    SERVERBOUND,
+    CLIENTBOUND
+}
+
+enum class PacketState {
+    HANDSHAKE,
+    STATUS,
+    LOGIN,
+    PLAY
+}
