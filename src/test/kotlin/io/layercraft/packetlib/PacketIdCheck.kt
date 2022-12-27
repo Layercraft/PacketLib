5e6a70f016f1ab33c830a1678b776df34d0d80ac
Julian Haag <me@jhaag.me>
2022-12-27 13:10:45 +0100

    Fix Tests

--- src/test/kotlin/io/layercraft/packetlib/PacketIdCheck.kt
+++ src/test/kotlin/io/layercraft/packetlib/PacketIdCheck.kt
@@ -1,17 +1,21 @@
 package io.layercraft.packetlib
 
-import io.layercraft.packetlib.codec.MinecraftCodecs
+import io.layercraft.packetlib.codec.MinecraftCodec
 import io.layercraft.packetlib.packets.MinecraftPacket
 import io.layercraft.packetlib.packets.PacketDirection
 import org.junit.jupiter.api.Test
 import kotlin.test.assertTrue
+import io.layercraft.packetlib.codec.MinecraftCodecs as MC
 
 class PacketIdCheck {
 
     @Test
     fun `check if all ids are same in registry and in packet class`() {
-        val codec = MinecraftCodecs.V_1_19_2
+        val codecs = listOf(MC.V1_9_4, MC.V1_19, MC.V1_19_2)
+        codecs.forEach { checkCodec(it) }
+    }
 
+    private fun checkCodec(codec: MinecraftCodec){
         codec.packets.forEach { (state, registry) ->
             registry.clientPacketMap.forEach { (id, packet) ->
                 val annotation = packet.packet.annotations.filterIsInstance<MinecraftPacket>().first()
