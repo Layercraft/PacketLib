3fb8479650fbfa3d989371ccb57263b2532c8345
Julian Haag <me@jhaag.me>
2023-08-01 03:50:50 +0200

    Remove Ktlint configurations and plugin
    Ktlint plugin and its corresponding configurations were removed from the build.gradle.kts and .editorconfig files.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     kotlin("jvm") version "1.8.20"
     id("org.jetbrains.dokka") version "1.8.20"
-    id("org.jlleitschuh.gradle.ktlint") version "11.4.0"
 }
 
 allprojects {
@@ -17,7 +16,6 @@ subprojects {
     apply {
         plugin("org.jetbrains.kotlin.jvm")
         plugin("org.jetbrains.dokka")
-        plugin("org.jlleitschuh.gradle.ktlint")
     }
 
     val dokkaPlugin by configurations
